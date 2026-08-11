7358ad458601c1bfec1f8c363404be1efbfcde68
Katherine Bargar <kbargar@fivecolleges.edu>
2026-08-11 13:07:21 +0000

    Process and yield the returned loans

--- src/folio_auto_renew/__init__.py
+++ src/folio_auto_renew/__init__.py
@@ -1,7 +1,10 @@
 """Module providing automatic renewals."""
 
+import json
+import re
 from collections.abc import AsyncIterator
-from typing import NamedTuple
+from functools import reduce
+from typing import Any, NamedTuple
 
 from folioclient import FolioClient
 
@@ -14,20 +17,89 @@ class Renewable(NamedTuple):
     source: str
 
 
-class AutoRenew:
-    """Class to provide loan renewal operations around a FolioClient."""
+def _prune_source(loan: dict[str, Any]) -> str:
+    return json.dumps(
+        {
+            "_".join(path): reduce(
+                lambda acc, key: acc.get(key) if isinstance(acc, dict) else None,
+                path,
+                loan,
+            )
+            for path in [
+                ("id"),
+                ("userId"),
+                ("borrower", "barcode"),
+                ("patronGroupAtCheckout", "name"),
+                ("borrower", "patronGroup"),
+                ("itemId"),
+                ("item", "barcode"),
+                ("item", "status", "name"),
+                ("item", "instanceId"),
+                ("item", "instanceHrid"),
+                ("itemEffectiveLocationIdAtCheckOut"),
+                ("item", "location", "name"),
+                ("loanDate"),
+                ("dueDate"),
+                ("loanPolicy", "name"),
+                ("dueDateChangedByRecall"),
+                ("dueDateChangedByNearExpireUser"),
+                ("dueDateChangedByHold"),
+                ("status", "name"),
+                ("metadata", "createdDate"),
+                ("metadata", "updatedDate"),
+            ]
+        },
+        indent=4,
+        default=str,
+    )
 
-    def __init__(self, client: FolioClient):
-        """Wraps the provide FolioClient with loan renewal operations."""
-        self._client = client
 
-    async def stream_renewables(self) -> AsyncIterator[Renewable]:
-        """Streams only renewable Loan information from FOLIO."""
-        for _ in range(5):
-            yield Renewable("", "", "")
+async def stream_renewables(
+    client: FolioClient,
+    patron_barcode_patterns: list[str],
+) -> AsyncIterator[Renewable]:
+    """Streams only renewable Loan information from FOLIO."""
+    patron_barcode_matcher = re.compile(rf"^({r'|'.join(patron_barcode_patterns)}).*$")
 
-    async def renew_many(self, renewables: AsyncIterator[Renewable]) -> None:
-        """Renews until the source Loans are exhausted."""
+    async for loan in client.folio_get_all_async(""):
+        source = _prune_source(loan)
 
-    async def renew_one(self, renewable: Renewable) -> None:
-        """Renews an individual Loan in FOLIO."""
+        patron_barcode = loan.get("borrower", {}).get("barcode")
+        if patron_barcode is None:
+            # TODO: Log the source
+            continue
+
+        if not patron_barcode_matcher.match(patron_barcode):
+            # Expected because it is for another campus
+            continue
+
+        if (
+            len(loan.get("itemId", "").strip()) == 0
+            or len(loan.get("userId", "").strip()) == 0
+        ):
+            # TODO: Log the source
+            continue
+
+        if loan.get("dueDateChangedByRecall", False):
+            # TODO: Log the source
+            continue
+
+        if loan.get(
+            "dueDateChangedByNearExpireUser",
+            False,
+        ) or loan.get(
+            "dueDateChangedByHold",
+            False,
+        ):
+            # TODO: Log the source with a warning but don't skip
+            ...
+
+        yield Renewable(loan["itemId"].strip(), loan["userId"].strip(), source)
+
+
+async def renew_many(client: FolioClient, renewables: AsyncIterator[Renewable]) -> None:
+    """Renews until the source Loans are exhausted."""
+
+
+async def renew_one(client: FolioClient, renewable: Renewable) -> None:
+    """Renews an individual Loan in FOLIO."""
