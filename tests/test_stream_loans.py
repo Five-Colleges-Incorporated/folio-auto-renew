959f81a8b23ab1abd677a42fdb2b587c81e32736
Katherine Bargar <kbargar@fivecolleges.edu>
2026-08-12 15:39:37 +0000

    Add first unit test with necessary dependency changes

---
+++ tests/test_stream_loans.py
@@ -0,0 +1,17 @@
+from datetime import datetime, timezone
+from unittest import mock
+
+import pytest
+
+
+@pytest.mark.asyncio
+async def test_no_loans() -> None:
+    from folio_auto_renew import stream_loans as uut
+
+    loan_results = mock.AsyncMock()
+    loan_results.__aiter__.return_value = []
+    client = mock.Mock()
+    client.folio_get_all_async.return_value = loan_results
+
+    loans = uut(client, [], datetime.now(tz=timezone.utc))
+    assert sum([1 async for _ in loans]) == 0
