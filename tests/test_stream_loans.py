eba520c9a584ede3f0a4f64a983f94e8fa2b2c61
Katherine Bargar <kbargar@fivecolleges.edu>
2026-08-12 16:01:54 +0000

    Switch to table driven tests

--- tests/test_stream_loans.py
+++ tests/test_stream_loans.py
@@ -1,55 +1,51 @@
 from datetime import datetime, timezone
+from typing import Any
 from unittest import mock
 
 import pytest
+from folioclient import FolioClient
+from pytest_cases import parametrize_with_cases
 
 
-@pytest.mark.asyncio
-async def test_no_loans() -> None:
-    from folio_auto_renew import stream_loans as uut
-
+def _arrange(loans: list[dict[str, Any]]) -> FolioClient:
     loan_results = mock.AsyncMock()
-    loan_results.__aiter__.return_value = []
+    loan_results.__aiter__.return_value = loans
     client = mock.Mock()
     client.folio_get_all_async.return_value = loan_results
 
-    loans = uut(client, [], datetime.now(tz=timezone.utc))
-    assert sum([1 async for _ in loans]) == 0
+    return client
 
 
-@pytest.mark.asyncio
-async def test_one_loan() -> None:
-    from folio_auto_renew import stream_loans as uut
+def case_no_loans() -> list[dict[str, Any]]:
+    return []
 
-    loan_results = mock.AsyncMock()
-    loan_results.__aiter__.return_value = [
+
+def case_one_loan() -> list[dict[str, Any]]:
+    return [
         {
             "itemId": "itemid",
             "userId": "userId",
             "borrower": {"barcode": "barcode-1234"},
         },
     ]
-    client = mock.Mock()
-    client.folio_get_all_async.return_value = loan_results
-
-    loans = uut(client, ["barcode"], datetime.now(tz=timezone.utc))
-    assert sum([1 async for _ in loans]) == 1
 
 
-@pytest.mark.asyncio
-async def test_many_loans() -> None:
-    from folio_auto_renew import stream_loans as uut
-
-    loan_results = mock.AsyncMock()
-    loan_results.__aiter__.return_value = [
+def case_many_loans() -> list[dict[str, Any]]:
+    return [
         {
             "itemId": "itemid",
             "userId": "userId",
             "borrower": {"barcode": "barcode-1234"},
         },
     ] * 5
-    client = mock.Mock()
-    client.folio_get_all_async.return_value = loan_results
 
-    loans = uut(client, ["barcode"], datetime.now(tz=timezone.utc))
-    assert sum([1 async for _ in loans]) == 5
+
+@pytest.mark.asyncio
+@parametrize_with_cases("exp_loans", cases=".")
+async def test_streamed_loans_count(exp_loans: list[dict[str, Any]]) -> None:
+    from folio_auto_renew import stream_loans as uut
+
+    client = _arrange(exp_loans)
+
+    act_loans = uut(client, [], datetime.now(tz=timezone.utc))
+    assert sum([1 async for _ in act_loans]) == len(exp_loans)
