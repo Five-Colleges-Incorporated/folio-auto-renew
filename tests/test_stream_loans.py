3c7e86921413ad6d59b94c1d56ad8670af2d443a
Katherine Bargar <kbargar@fivecolleges.edu>
2026-08-12 15:47:53 +0000

    Final sanity check for multiple loans

--- tests/test_stream_loans.py
+++ tests/test_stream_loans.py
@@ -34,3 +34,22 @@ async def test_one_loan() -> None:
 
     loans = uut(client, ["barcode"], datetime.now(tz=timezone.utc))
     assert sum([1 async for _ in loans]) == 1
+
+
+@pytest.mark.asyncio
+async def test_many_loans() -> None:
+    from folio_auto_renew import stream_loans as uut
+
+    loan_results = mock.AsyncMock()
+    loan_results.__aiter__.return_value = [
+        {
+            "itemId": "itemid",
+            "userId": "userId",
+            "borrower": {"barcode": "barcode-1234"},
+        },
+    ] * 5
+    client = mock.Mock()
+    client.folio_get_all_async.return_value = loan_results
+
+    loans = uut(client, ["barcode"], datetime.now(tz=timezone.utc))
+    assert sum([1 async for _ in loans]) == 5
